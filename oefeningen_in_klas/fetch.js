0d1a891d560117f6478bf5d43489de7f34a2b4d3
JorenVsyntra <vandervelden.joren@gmail.com>
2024-11-12 11:45:05 +0100

    oef ingezet

--- oefeningen_in_klas/fetch.js
+++ oefeningen_in_klas/fetch.js
@@ -1,7 +1,23 @@
 const url = 'https://jsonplaceholder.typicode.com/todos';
 fetch(url)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    
+})
 .catch(e => console.log(e));
 
 // HTTP VERB (GET) (POST,DELETE,PUT)
+
+// SUBPROBLEMS
+
+// create a basic HTML structure
+// create a JS file with logic
+// find endpoint api
+// consume endpoint api
+// display data in HTML using a iteration
+        // make iteration
+        // create HTML node (id)
+        // node.innerHTML (+=)
+// analyse array of objects structure (wich attributes are available)
+// Done ;
+
