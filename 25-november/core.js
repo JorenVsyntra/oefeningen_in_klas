08b4f9a707f3e1632cacb19e646711d56589a4c5
JorenVsyntra <vandervelden.joren@gmail.com>
2024-11-26 10:02:35 +0100

    laatste oef

--- 25-november/core.js
+++ 25-november/core.js
@@ -60,3 +60,37 @@ document.getElementById("checkFruitButton").addEventListener("click", function()
     const resultText = result ? "The array contains fruits!" : "No fruits found in the array.";
     document.getElementById("result").innerText = resultText;
 });
+
+
+
+// Why Is Array/Object Destructuring So Useful And How To Use It
+// https://www.youtube.com/watch?v=NIq3qLaHCIs
+// Destructuring is a way to extract data from an array or object
+// and assign it to a variable
+// Destructuring an array
+
+
+let myArray3 = ["apple", "banana", "carrot"];
+let [fruitD1, fruitD2, fruitD3] = myArray3;
+console.log(fruitD1); // apple
+
+// Destructuring an object
+let myObject = {
+    fruitname: "apple",
+    price: 1.99,
+    quantity: 5,
+    category: "fruit",
+    origin: {
+        country: "USA",
+        state: "California"
+    }
+};
+
+let { fruitname, price, ...rest} = myObject;
+let { country, state } = myObject.origin;
+let { origin: { country: country2, state: state2 } } = myObject;
+console.log(rest); // apple
+console.log (country); // USA
+console.log (state); // California 
+console.log (country2); // USA
+console.log (state2); // California
