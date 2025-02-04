cb824c7250af96f50205a478ec1a478f7a848208
JorenVsyntra <vandervelden.joren@gmail.com>
2025-02-04 17:23:28 +0100

    something

---
+++ back/php/concat.php
@@ -0,0 +1,17 @@
+<?php 
+// Concatenation
+$first_name = "John";
+$last_name = "Doe";
+
+echo $first_name . " " . $last_name;
+
+$a = 1;
+$b = "two";
+$c = [3,4,5];
+$d = true;
+$e = null;
+
+echo $a . $b . $c . $d . $e;
+
+?>
+```
--- back/php/index.php
+++ back/php/index.php
@@ -1,4 +1,14 @@
 <?php
-echo "Hello World";
-echo "<b>Hello World</b>";
-echo "<h1>Hello World</h1>";
+// Create 5 variables with different string values
+$name = "John";
+$city = "New York";
+$job = "software engineer";
+$hobby = "chess";
+$age = "35";
+
+// Concatenate variables into a sentence and store in a final variable
+$fullSentence = $name . " is a " . $job . " living in " . $city . " who enjoys playing " . $hobby . " and is " . $age . " years old.";
+
+// Display the sentence
+echo $fullSentence;
+?>
